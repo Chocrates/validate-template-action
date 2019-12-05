02664d1b2d05066386e5ccf4c754554a055971d5
Chris McIntosh <chocrates@github.com>
2019-12-05 17:34:40 -0600

    Does a global work?

--- runner.js
+++ runner.js
@@ -1,19 +1,20 @@
+options = {}
 module.exports =  class Runner {
     constructor(_options) {
-        this.options = _options
+        options = _options
     }
 
     async run(){
         try {
             console.log('Testing...')
-            console.log(this.options)
+            console.log(options)
             console.log('Testing...')
-            const octokit = new this.options.github.GitHub(this.options.token)
+            const octokit = new options.github.GitHub(options.token)
 
-            this.options.core.debug(`Starting Parsing on labels ${this.options.labels.join(',')}`)
+            options.core.debug(`Starting Parsing on labels ${options.labels.join(',')}`)
 
-            this.options.core.debug({payload: JSON.stringify(this.options.github.context.payload)})
-            console.log(this.options.github.context.payload)
+            options.core.debug({payload: JSON.stringify(options.github.context.payload)})
+            console.log(options.github.context.payload)
 
         } catch ( error ) {
             this.options.core.setFailed(`Failure ${error}`) 
