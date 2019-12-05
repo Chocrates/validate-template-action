70439b7ac4a7561f63023fd05c7379f2efeb1b17
Chris McIntosh <chocrates@github.com>
2019-12-05 15:42:49 -0600

    dumpin gissue data

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ run = async () => {
     try {
         const labels = core.getInput('ISSUE_LABELS').split(',')
         const template = core.getInput('ISSUE_TEMPLATE')
+        const token = process.env['GITHUB_TOKEN']
+        const octokit = new github.GitHub(token)
 
         core.debug(`Starting Parsing on labels ${labels.join(',')}`)
+
+        core.debug({payload: github.context.payload})
     } catch ( error ) {
         core.setFailed(`Failure ${error}`) 
     }
